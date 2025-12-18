68771c8f96e058285d89529f1926cf6aec6f74f3
Adam Hakla <adamh@example.com>
2025-12-18 11:19:34 +0100

    excluded node_modules and e2e tests

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: [
+      'src/tests/**/*.test.js'
+    ],
     exclude: [
+      'node_modules/**',
       'src/tests/e2e/**'
     ],
   },
